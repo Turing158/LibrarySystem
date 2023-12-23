5842d140906e376b27ee147bfe426771509b783e
Turing158 <15818961209@163.com>
2023-12-23 17:55:09 +0800

    init

--- src/main/java/com/libSystem/config/webConfig.java
+++ src/main/java/com/libSystem/config/webConfig.java
@@ -1,5 +1,7 @@
 package com.libSystem.config;
 
+import org.apache.catalina.filters.RemoteIpFilter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,5 +11,9 @@ public class webConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry){
         registry.addResourceHandler("/**")
                 .addResourceLocations("classpath:/templates/", "classpath:/templates/");
+    }
+    @Bean
+    public RemoteIpFilter remoteIpFilter(){
+        return new RemoteIpFilter();
     }
 }
--- src/main/java/com/libSystem/controller/BookController.java
+++ src/main/java/com/libSystem/controller/BookController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -16,6 +17,11 @@ public class BookController {
     @Autowired
     BookService bookService;
 
+
+    @RequestMapping("/book")
+    public String bookList(){
+        return "bookList";
+    }
 
     @PostMapping("/findAllBook")
     @ResponseBody
@@ -78,4 +84,6 @@ public class BookController {
     public Result insertBook(@RequestBody Book book){
         return bookService.insertBook(book);
     }
+
+
 }
